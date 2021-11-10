4ebbcff5a7341352dae807d28e52d00e660257b7
MomoTori <813424850@qq.com>
2021-11-10 12:41:55 +0800

    add functions,change parameter of functions to reduce codes

--- Elevator/Simulation.cpp
+++ Elevator/Simulation.cpp
@@ -10,7 +10,8 @@ extern const int T_Into_F;       //乘客进楼时间间隔上限
 extern const int EWT;            //电梯等待时间
 extern const int WaitingTime;    //乘客等待时间
 extern const int MovingT;        //电梯移动一楼所用时间
-
+extern const int Doortime;
+extern const int CloseDoorWaitingtime;
 State::State(Evt *p)
 {
     memset(Callup, 0, sizeof(Callup));
@@ -21,32 +22,106 @@ State::State(Evt *p)
     srand(time((time_t *)NULL));
 }
 
-void State::EvaMove(int n, int floor)
+void State::EvaMove(int eva, elevator::state flag)
 {
-    
+    //在此时便改变电梯楼层
+    //用一个整数队列存储移动的顺序
+    if (e->EvtTraverse(EvaMove, eva) && !E[eva].IfIO) //n号电梯没有移动&&未开门
+    {
+        if (flag == elevator::Up)
+            E[eva].floor++;
+        if (flag == elevator::Down)
+            E[eva].floor--;
+        e->AddEvt(e->time() + MovingT, EvaMoveDone, eva);
+    }
+}
+void State::EvaMoveDone(int eva)
+{
+    //若有电梯停止事件则触发停止事件：先判断到达指定楼层，然后判断该层有同方向call
+    if (E[eva].floor == E[eva].targetfloor || (E[eva].S == elevator::Up && Callup[eva]) || (E[eva].S == elevator::Down && Calldown[eva]))
+    {
+        E[eva].IFDO = 1;
+        e->AddEvt(e->time() + Doortime, FBIOpenTheDoor, eva);
+    }
+    else
+        EvaMove(eva, E[eva].S);
+}
+void State::FBIOpenTheDoor(int eva)
+{
+    e->AddEvt(e->time(), IOEva, eva);
+    e->AddEvt(e->time() + CloseDoorWaitingtime, CloseDoor, eva);
+}
+void State::IOEva(int eva)
+{
     /////////////////
     //Wait for make//
     /////////////////
+    E[eva].IfIO = 1;
 }
-void State::EvaMoveDone()
+void State::CloseDoor(int eva)
 {
-    //若有电梯停止事件则触发停止事件：先判断到达指定楼层，然后判断该层有同方向call
     //若已达到电梯target，则判断其新的target
     //若无则根据target设置下一个move
     //若无下一个move改变电梯状态为停止，并设置电梯返回本垒层事件
-    /////////////////
-    //Wait for make//
-    /////////////////
+    if (E[eva].IfIO)
+    {
+        E[eva].IfIO = 0;
+        e->AddEvt(e->time() + CloseDoorWaitingtime, CloseDoor, eva); //设置下一次检查
+        return;
+    }
+    if (E[eva].IFDO)
+    {
+        e->AddEvt(e->time() + Doortime, CloseDoor, eva); //设置关门完成事件
+        E[eva].IFDO = 0;
+        return;
+    }
+    
+    ///////////////////////
+    //We'll be right back//
+    ///////////////////////
+    //若当楼call有同方向则再次呼叫FBIOpenTheDoor
 }
-void State::CallEva()
+
+void State::HomeSweetHome(int eva)
 {
-    //若有电梯现楼层到target楼层有呼叫楼层，则
-    //若有停止电梯，改变其target为该层
-    /////////////////
-    //Wait for make//
-    /////////////////
+    ;
+    //       //=====================|
+    //      ‹‹To be continued...\\//|
+    //       \\=====================|
 }
-void State::PIF()
+void State::CallEva(int floor, elevator::state flag) //根据呼叫楼层和上下楼标识呼叫电梯
+{
+    //若存在电梯现楼层到target楼层有呼叫楼层，则不做任何动作
+    //若有停止电梯，改变其target为该层
+    //若无不做任何动作
+    int stopE = -1, i = 0;
+    while (i < ElevatorNumber)
+    {
+        if (E[i].S == flag && (E[i].floor - floor) * (floor - E[i].targetfloor) >= 0)
+            return;
+        if (E[i].S == elevator::Stop)
+            stopE = i;
+        i++;
+    }
+    if (i != -1)
+    {
+        if (floor > E[stopE].floor)
+        {
+            E[stopE].S = elevator::Up;
+            E[stopE].targetfloor = floor;
+            EvaMove(stopE, elevator::Up);
+        }
+        if (floor < E[stopE].floor)
+        {
+            E[stopE].S = elevator::Down;
+            E[stopE].targetfloor = floor;
+            EvaMove(stopE, elevator::Down);
+        }
+        if (floor == E[stopE].floor)
+            FBIOpenTheDoor(floor);
+    }
+}
+void State::PIF(int i = 0)
 {
     e->AddEvt(e->time() + rand() % T_Into_F + 1, State::PIF); //下一个人进入事件加入事件表，其中加1牺牲一个时间精度消除同时有人进楼的可能
     int floor = rand() % FLOOR;
@@ -63,12 +138,12 @@ void State::PIF()
     if (target > floor && !Callup[floor])       //上楼且未按按钮
     {
         Callup[floor] = 1;
-        CallEva();
+        CallEva(floor, elevator::Up);
     }
     if (target < floor && !Calldown[floor])
     {
         Calldown[floor] = 1;
-        CallEva();
+        CallEva(floor, elevator::Down);
     }
     if (!e->EvtTraverse(RushB)) //没有人想离开，即这是第一个人
         setRushB();
@@ -79,9 +154,10 @@ void State::PIF()
         std::cout << e->time() << "t:\t"
                   << "-1楼进入了1个人.\n";
 }
-void State::setRushB() //空楼进人或原Rush事件结束后调用，生成rush事件
+void State::setRushB(int i = 0) //空楼进人或原Rush事件结束后调用，生成rush事件
 {
-    int i = 0, min;
+    i = 0;
+    int min;
     while (i < FLOOR && !PInF[i].next) //跳过没有人的楼层
         i++;
     min = i++;
@@ -94,9 +170,10 @@ void State::setRushB() //空楼进人或原Rush事件结束后调用，生成rus
     if (min < FLOOR)
         e->AddEvt(PInF[min].next->giveuptime, RushB); //若有人则设置rushb事件
 }
-void State::RushB()
+void State::RushB(int i)
 {
-    int i = 0, min;
+    i = 0;
+    int min;
     while (i < FLOOR && !PInF[i].next) //跳过没有人的楼层
         i++;
     min = i++;
@@ -121,27 +198,31 @@ void State::RushB()
     setRushB();
 }
 
-void Evt::AddEvt(int t, auto foo) //添加事件
+void Evt::AddEvt(int t, auto foo, int n = 0) //添加事件
 {
     event *temp = new event(t, foo), *pt = &Head;
     while (pt->next && temp->time > pt->next->time)
         pt = pt->next;
     temp->next = pt->next;
     pt->next = temp;
+    temp->n = n;
 }
 void Evt::EvtHappen()
 {
     auto temp = Head.next;
     Head.time = temp->time;
     Head.next = temp->next;
-    (s->*(temp->Finish))(); //调用s对应的函数
+    (s->*(temp->Finish))(temp->n); //调用s对应的函数
     delete temp;
 }
-bool Evt::EvtTraverse(auto foo)
+bool Evt::EvtTraverse(auto foo, int i = -1)
 {
     auto pt = &Head;
-    while (pt->next && pt->next->Finish != foo)
-        pt = pt->next;
+    while (pt->next)
+        if (pt->next->Finish == foo && (i == -1 || pt->next->n == i))
+            break;
+        else
+            pt = pt->next;
     if (pt->next)
         return 1;
     return 0;
--- Elevator/Simulation.h
+++ Elevator/Simulation.h
@@ -1,10 +1,13 @@
 #include <iostream>
 const int FLOOR = 5;
 const int ElevatorNumber = 2; //电梯数
-const int T_Into_F = 100;     //乘客进楼时间间隔上限
+const int T_Into_F = 200;     //乘客进楼时间间隔上限
 const int EWT = 300;          //电梯等待时间
 const int WaitingTime = 400;  //乘客等待时间
 const int MovingT = 70;       //电梯移动一楼所用时间
+const int Doortime = 20;      //开关门时间
+const int CloseDoorWaitingtime = 40;//关门测试时间
+
 
 class Evt;
 
@@ -21,12 +24,11 @@ struct elevator
         Stop,
         Up,
         Down
-    }; //电梯状态
-    state S;
+    } S; //电梯状态
     int floor;
     people *passenger; //按照最近的目标楼层排序
     int targetfloor;   //乘客前往的最近的targetfloor
-    bool IfDO;         //door open,是否开门,1开门
+    bool IFDO;         //是否开门，判断是否停靠
     bool IfIO;         //开门后是否有人进出电梯
     elevator()
     {
@@ -34,7 +36,7 @@ struct elevator
         floor = 1;
         passenger = NULL;
         targetfloor = 1;
-        IfDO = 0;
+        IFDO = 0;
         IfIO = 0;
     }
 };
@@ -45,25 +47,30 @@ private:
     bool Callup[FLOOR];   //人的上楼标记
     bool Calldown[FLOOR]; //人的下楼标记
     people PInF[FLOOR];   //People in Floor,各楼的人的头链表，按照放弃时间排序
-    elevator *E;
-    Evt *e;
+    elevator *E;          //电梯
+    Evt *e;               //事件连接
 
 public:
     State(Evt *p);
-    void EvaMove(int n, int floor); //电梯移动命令，对第n个电梯下达前往Floor楼的命令,n>=0
-    void EvaMoveDone();             //事件函数，移动结束事件
-    void CallEva();                 //呼叫电梯命令，发起电梯调配
-    void PIF();                     //事件函数，People Into Floor乘客进楼事件
-    void setRushB();                //命令，设置RushB事件
-    void RushB();                   //事件函数，判断有没有人等不及润了
+    void EvaMove(int n, elevator::state flag);     //电梯移动命令，对第n个电梯下达上楼或下楼的命令,n>=0
+    void EvaMoveDone(int);                         //事件函数，移动结束事件
+    void CallEva(int floor, elevator::state flag); //呼叫电梯命令，发起电梯调配
+    void FBIOpenTheDoor(int);                      //电梯开门及之后的事件处理
+    void IOEva(int);//乘客进出电梯，先出后进
+    void CloseDoor(int);//关闭电梯，寻找下一个target楼层，执行EvaMove.
+    void HomeSweetHome(int); //电梯开始返回本垒层
+    void PIF(int);           //事件函数，People Into Floor乘客进楼事件
+    void setRushB(int);      //命令，设置RushB事件
+    void RushB(int);         //事件函数，判断有没有人等不及润了
 };
 
 struct event
 {
-    int time;                //事件发生时间
-    event *next;             //组成事件链表
-    void (State::*Finish)(); //事件的动作函数，并可用于标识事件类型
-    event(int t, void (State::*p)())
+    int time;                   //事件发生时间
+    event *next;                //组成事件链表
+    void (State::*Finish)(int); //事件的动作函数，并可用于标识事件类型,若函数无参数则使用默认参数吸收保证形式相同
+    int n;                      //保存传入参数
+    event(int t, void (State::*p)(int))
     {
         time = t;
         next = NULL;
@@ -77,10 +84,10 @@ private:
     State *s;   //绑定State
 public:
     Evt(State *a) : Head(event(0, NULL)) { s = a; }
-    void AddEvt(int t, auto foo);
+    void AddEvt(int t, auto foo, int n);
     void EvtHappen();
-    bool EvtTraverse(auto foo); //遍历是否有foo对应事件,若有返回1
-    void EvtDelete(auto foo);   //删除触发函数为foo的第一个事件.
+    bool EvtTraverse(auto foo, int i); //遍历是否有foo对应事件,若有返回1
+    void EvtDelete(auto foo);          //删除触发函数为foo的第一个事件.
     int time() { return Head.time; }
 };
 
